24f3656f380ccceeeb49c1e24454a42508bcf546
Zakris Stromqvist <zakrisstromqvist@gmail.com>
2023-04-25 20:51:50 +0200

    add stuff

--- game/stuff.py
+++ game/stuff.py
@@ -3,3 +3,15 @@ date_var = pd.to_datetime(df['date_of_birth'])
 
 # Use the date variable to filter the DataFrame
 filtered_df = df[date_var < '1990-01-01']
+
+# Create a specific date
+my_date = pd.Timestamp('1990-01-01')
+
+# Use the date to filter the DataFrame
+filtered_df = df[date_var < my_date]
+
+
+date_string = "2023-04-25"
+date = datetime.strptime(date_string, "%Y-%m-%d").date()
+
+print(date)  # Output: 2023-04-25
